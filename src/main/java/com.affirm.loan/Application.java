fb2016cb8bdf53e56a05e5c30d56385464b99d44
PriyankaMutyala <priyanka.mutyala@target.com>
2022-04-06 03:39:04 -0500

    upgraded java version and auto run

--- src/main/java/com.affirm.loan/Application.java
+++ src/main/java/com.affirm.loan/Application.java
@@ -1,12 +1,16 @@
 package com.affirm.loan;
 
+import com.affirm.loan.service.AssignLoanService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+        AssignLoanService assignLoanService = applicationContext.getBean(AssignLoanService.class);
+        assignLoanService.assignLoan();
     }
 }
